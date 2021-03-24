6e0ee90be75c0679d269db3a9c8ae4f20e44a1d5
uwadonat <uwadonat@gmail.com>
2021-03-23 23:11:21 -0400

    create users"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
-   def index
+   def new
+      @user = User.new
    end
 
    def create
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+      if @user.save
+        redirect_to new_users_path
+      else
+        render :new
+      end
    end
 end
