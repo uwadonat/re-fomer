0a88384b338726d299d0f3710fff357d463f6d79
uwadonat <uwadonat@gmail.com>
2021-03-25 05:46:37 -0400

    build a form with #form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
    end
 
    def create
-      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-      # @user = User.new(user_params)
+      # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user = User.new(user_params)
 
       if @user.save
         redirect_to new_users_path
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
        render :new
       end
    end
-
+   
+   private
    def user_params
       params.require(:user).permit(:username,:email,:password)
    end
