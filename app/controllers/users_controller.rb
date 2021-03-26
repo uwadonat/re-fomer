d918bf04794c2d0b74dc35ef5291d7d9abef58ee
uwadonat <uwadonat@gmail.com>
2021-03-26 07:25:30 -0400

    add error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
 
       if @user.save
-        redirect_to new_user_path
+        redirect_to @user
       else
        render :new
       end
@@ -32,7 +32,14 @@ class UsersController < ApplicationController
       @user = User.all
     end
 
-     
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+  
+      redirect_to @user
+    end
+
+   
    private
    def user_params
       params.require(:user).permit(:username,:email,:password)
