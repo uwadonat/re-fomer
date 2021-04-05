f98f221b45ba2ca4e6715a89389c2c4f7d006990
uwadonat <uwadonat@gmail.com>
2021-04-05 11:09:00 -0400

    delete commented code and change path after create new user and update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    @user = User.new(user_params)
+     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user
+      redirect_to new_user_path(@user)
     else
       render :new
     end
@@ -22,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      redirect_to @user
+      redirect_to edit_user_path(@user)
     else
       render :edit
     end
